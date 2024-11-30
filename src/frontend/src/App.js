10b9df0d9c1fa516ad7afc9a00e7622c8aa67746
LuigiBorgesLa <luigiuchiha10@gmail.com>
2024-11-30 14:00:36 -0300

    criação dashboard 2

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './paginas/login'; // Caminho do arquivo Login
 import Dashboard from './paginas/Dashboard'; // Caminho do arquivo Dashboard
+import Dashboard2 from './paginas/Dashboard2';
 import UserPage from './paginas/userpage';
 import TeacherPage from './paginas/TeacherPage';
 import StudentPage from './paginas/StudentPage';
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard2" element={<Dashboard2 />} />
         <Route path="/userpage" element={<UserPage />} />
         <Route path="/techerpage" element={<TeacherPage/>} />
         <Route path="/StudentPage" element={<StudentPage/>} />
--- src/frontend/src/paginas/login.js
+++ src/frontend/src/paginas/login.js
@@ -5,37 +5,25 @@ import './css/login.css';
 function Login() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
-  const [nivel, setNivel] = useState(''); // Adicionando o estado para o nível
   const [erro, setErro] = useState('');
-  const [logado, setLogado] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Validação de login
-    if (email === 'ad@g.com' && senha === '123' && nivel === '1') {
-      setLogado(true);
+    if (email === 'joao@gmail.com' && senha === '123') {
+      localStorage.setItem('userLevel', '1');
       setErro('');
-      navigate('/dashboard'); // Redireciona para o Dashboard de administrador
-    } else if (email === 'user@g.com' && senha === '123' && nivel === '2') {
-      setLogado(true);
+      navigate('/dashboard');
+    }
+    else if (email === 'lucas@gmail.com' && senha === '123') {
+      localStorage.setItem('userLevel', '2');
       setErro('');
-      navigate('/userpage'); // Redireciona para a página do usuário
+      navigate('/dashboard2');
     } else {
-      setErro('Usuário, senha ou nível inválidos.');
-      setLogado(false);
+      setErro('Usuário ou senha inválidos.');
     }
   };
-
-  if (logado) {
-    return (
-      <div className="login-container">
-        <h1>Bem-vindo, {email}!</h1>
-        <p>Você está logado.</p>
-      </div>
-    );
-  }
 
   return (
     <div className="login-container">
@@ -62,18 +50,6 @@ function Login() {
               placeholder="Digite sua senha"
               required
             />
-          </div>
-          <div className="input-group">
-            <label>Nível:</label>
-            <select
-              value={nivel}
-              onChange={(e) => setNivel(e.target.value)}
-              required
-            >
-              <option value="">Selecione o nível</option>
-              <option value="1">Administrador</option>
-              <option value="2">Usuário</option>
-            </select>
           </div>
           <button type="submit" className="btn">Entrar</button>
         </form>
